e861a310ac274962f872dcb2b321154c9797e136
Boluwatife Gbadamosi <gbadamosiboluwatife2002@gmail.com>
2025-08-11 16:14:31 +0100

    ass

---
+++ String_operations/task3.py
@@ -0,0 +1,31 @@
+# name = input("Kindly input your name: ")
+# print(name.upper())
+
+# name = "python"
+# print(name[:1])
+# print(name[5:])
+
+# name = input("What is your name: ")
+# print("Hello!," + " " + name)
+
+# food = "rice and beans"
+# print(food.find("s"))
+
+# text = "Hello World"
+# print(text.replace("World", "Python"))
+
+# task2-6
+# name = "I am learinig python as an AI developer"
+# print("python" in name) 
+
+# Number 7
+name = "Boluwatife"
+print(name[11:0])
+# name = "      Boluwatife"
+# print(name.lstrip())
+
+
+# name = "Boluwatife    "
+# print(name.rstrip())
+# text = input("Enter a sentence:   ")
+# vowels =('a','e','i',)
---
+++ String_operations/working_with_string.py
@@ -0,0 +1,66 @@
+
+# word = "Python"
+# print(word[0]) # P
+# print(word[-1]) # n
+# print(word[1]) # n
+
+# word = "Python"
+# print(word[0:4])
+# print(word[2:])
+# print(word[:3])
+# print(word[::4])
+# print(word[::-1])
+
+# a = "Hello"
+# b = "World"
+# print(a + " " + b)
+
+# word = "Hi! "
+# print(word * 3)
+
+# text = "Python programming"
+# print("Python" in text)
+# print("Java" not in text)
+
+# text = "Hello World"
+# print(text.index("World"))
+
+# filename = "data.csv"
+# print(filename.startswith("data"))
+# print(filename.endswith(".csv"))
+
+# Converts all the characters into uppercase using upper()
+name = "Ada Balogun"
+print(name.upper())
+
+# Using lower() to convert string to lowercase
+sentence = "python is amazing"
+print(sentence.title())
+
+# Removing whitespaces using strip()
+text = "   Abuja"
+print(text.strip())
+
+# Replacing occurences of substring with another substring using replace()
+message = "I love Java"
+print(message.replace("Java", "Python"))
+
+# switching lowercase to uppercase using swapcase()
+text = "Hello ABEOKUTA"
+print(text.swapcase())
+
+# removing whitespace or characters from the leftside only
+text = "    Nigeria"
+print(text.lstrip())
+
+# Splits a string into a list using a seperator 
+fruits = "mango orange banana"
+print(fruits.split())
+
+#spliting a string into a list from the right side using rsplit()
+text = "one, two, three, four"
+print(text.rsplit("," , 1))
+
+
+# Spliting a string into a list at each new line \n
+lines = "Line 1\n"
